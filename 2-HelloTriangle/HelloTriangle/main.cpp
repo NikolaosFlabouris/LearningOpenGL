ac0d588ffa8990ced4c9828d6ee966684bdd9a17
Wingman953 <nikolaosflabouris@gmail.com>
2019-09-17 13:29:53 +0930

    Improved formatting for HelloTriangle.

--- 2-HelloTriangle/HelloTriangle/main.cpp
+++ 2-HelloTriangle/HelloTriangle/main.cpp
@@ -19,7 +19,7 @@ const unsigned int SCR_HEIGHT = 600;
 unsigned int vertexVaoHandle;
 
 // Handle to our shader program.
-unsigned int programID;
+unsigned int shaderID;
 
 ///
 /// Process all input by querying GLFW whether relevant keys are pressed/released
@@ -27,7 +27,7 @@ unsigned int programID;
 ///
 /// \param window - the active GLFW Window.
 ///
-void processInput(GLFWwindow* window)
+void ProcessInput(GLFWwindow* window)
 {
     // Escape key closes window.
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -44,30 +44,27 @@ void processInput(GLFWwindow* window)
 /// \param width - the new width of the window.
 /// \param height - the new height of the window.
 ///
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
     // Set the OpenGL viewport to be that of the new window size.
     glViewport(0, 0, width, height);
 }
 
-/**
- * Sets the shader uniforms and vertex data
- * This happens ONCE only, before any frames are rendered
- * @returns 0 for success, error otherwise
- */
-int setVertexData()
+///
+/// Sets the shader uniforms and vertex data. This happens ONCE only, before any frames are rendered.
+///
+/// \return - 0 for success, error otherwise
+///
+int SetVertexData()
 {
-    /*
-     * What we want to draw
-     * Each set of 3 vertices (9 floats) defines one triangle
-     * You can define more triangles to draw here
-     */
+    // Set of 3 vertices (9 floats) defining one triangle.
     float vertices[] = {
          -0.5f, -0.5f, 0.0f, // left  
           0.5f, -0.5f, 0.0f, // right 
           0.0f,  0.5f, 0.0f  // top   
     };
 
+    // Colours to be rendered at each vertex.
     float colours[] = {
             1.0, 0.0, 0.0, 1.0,
             0.0, 1.0, 0.0, 1.0,
@@ -79,24 +76,30 @@ int setVertexData()
     glGenVertexArrays(1, &vertexVaoHandle);
     glBindVertexArray(vertexVaoHandle);
 
-    // Generate new buffers in our VAO
-    // A single data buffer store for per-vertex attributes (e.g. position).
-    unsigned int buffer[2];
-    glGenBuffers(2, buffer);
+    // Generate 2 new buffers in our VAO to store per-vertex attributes.
+    // One buffer to store vertex positions and another to store colours, read by shaders.
+    unsigned int vertexBuffer;
+    unsigned int colourBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glGenBuffers(1, &colourBuffer);
 
     // Allocate GPU memory for our vertices and copy them over.
-    glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Now we tell OpenGL how to interpret the data we just gave it.
-    // Tell OpenGL what shader variable it corresponds to.
-    // Tell OpenGL how it's formatted (floating point, 3 values per vertex).
+    // Tell OpenGL what shader variable it corresponds to (location = 0)
+    // and how it is formatted (floating point, 3 values per vertex).
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, VALS_PER_VERT, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
+    // Allocate GPU memory for our colours and copy them over.
+    glBindBuffer(GL_ARRAY_BUFFER, colourBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
 
+    // Now we tell OpenGL how to interpret the data we just gave it.
+    // Tell OpenGL what shader variable it corresponds to (location = 1)
+    // and how it is formatted (floating point, 4 values per vertex).
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, VALS_PER_COLOUR, GL_FLOAT, GL_FALSE, 0, 0);
 
@@ -107,46 +110,45 @@ int setVertexData()
     // The same is true for buffers, so we unbind it too.
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    return 0;	// return success
+    return 0;	// Return success.
 }
 
-/**
- * Renders a frame of the state and shaders we have set up to the window
- * Executed each time a frame is to be drawn.
- */
-void render()
+///
+/// Render, to be called every frame.
+///
+void Render()
 {
     // Clear the previous pixels we have drawn to the colour buffer (display buffer)
-    // Called each frame so we don't draw over the top of everything previous
+    // and depth buffer. Called each frame so we don't draw over the top of everything previous.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Specify the shader program we want to use
-    glUseProgram(programID);
+    // Specify the shader program we want to use.
+    glUseProgram(shaderID);
 
-    // Make the VAO with our vertex data buffer current
+    // Make the VAO with our vertex data buffer current.
     glBindVertexArray(vertexVaoHandle);
 
-    // Send command to GPU to draw the data in the current VAO as triangles
+    // Send command to GPU to draw the data in the current VAO as triangles.
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glFlush();	// Guarantees previous commands have been completed before continuing
+    glFlush();	// Guarantees previous commands have been completed before continuing.
 }
 
-/**
- * Error callback for GLFW. Simply prints error message to stderr.
- */
-static void error_callback(int error, const char* description)
+///
+/// Error callback for GLFW. Simply prints error message to stderr.
+///
+static void ErrorCallback(int error, const char* description)
 {
     fputs(description, stderr);
 }
 
-/**
- * Program entry. Sets up OpenGL state, GLSL Shaders and GLFW window and function call backs
- * Takes no arguments
- */
+///
+/// Renders a triangle in the centre of the screen. The 3 primary colours are assigned
+/// to the vertices and the shader interpolates the internal colours from there.
+///
 int main(int argc, char** argv)
 {
-    glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(ErrorCallback);
 
     // GLFW initialisation.
     if(!glfwInit())
@@ -170,7 +172,7 @@ int main(int argc, char** argv)
     glfwMakeContextCurrent(window);
 
     // Callback to change OpenGL viewport size as window size changes.
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
     // Load all GLAD OpenGL function pointers.
     if(!gladLoadGLLoader(( GLADloadproc) glfwGetProcAddress))
@@ -183,15 +185,15 @@ int main(int argc, char** argv)
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
     // Set up the shaders we are to use. 0 indicates error.
-    programID = ShaderUtils::LoadShaders("minimal.vert", "minimal.frag");
-    if(programID == 0)
+    shaderID = ShaderUtils::LoadShaders("minimal.vert", "minimal.frag");
+    if(shaderID == 0)
     {
         std::cout << "Failed to load shaders." << std::endl;
         exit(1);
     }
 
     // Set the vertex data for the program
-    if(setVertexData() != 0)
+    if(SetVertexData() != 0)
     {
         std::cout << "Failed to set vertex data." << std::endl;
         exit(1);
@@ -202,9 +204,9 @@ int main(int argc, char** argv)
     // Windows are double buffered, so need to swap buffers.
     while(!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        ProcessInput(window);
 
-        render();
+        Render();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
