fa693f5c1675f9d26278befbd74157ca2f5d95a9
Wingman953 <nikolaosflabouris@gmail.com>
2019-09-17 10:05:28 +0930

    Finally reorganised to a slightly better folder structure.

---
+++ 1-HelloWindow/HelloWindow/main.cpp
@@ -0,0 +1,98 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+// Function signatures.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Screen size.
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+///
+/// The 'Hello World' of OpenGL, a single window rendering one colour that
+/// closes upon pressing the escape key.
+///
+int main()
+{
+    // GLFW initialisation.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Instantiate GLFW window with screen resolution and title.
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Callback to change OpenGL viewport size as window size changes.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Load all GLAD OpenGL function pointers.
+    if(!gladLoadGLLoader(( GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Render loop, keep rendering until window is closed.
+    while(!glfwWindowShouldClose(window))
+    {
+        // Process any relevant user input.
+        processInput(window);
+
+        // Render.
+        // Set clear colour and then clear the colour buffer by applying that colour everywhere.
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // GLFW poll IO events (keys pressed/released, mouse moved etc.).
+        glfwPollEvents();
+
+        // GLFW swap buffers.
+        glfwSwapBuffers(window);
+    }
+
+    // GLFW clean up, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+///
+/// Process all input by querying GLFW whether relevant keys are pressed/released
+/// this frame and react accordingly.
+///
+/// \param window - the active GLFW Window.
+///
+void processInput(GLFWwindow* window)
+{
+    // Escape key closes window.
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+///
+/// GLFW callback whenever the window size changed (by OS or user resize)
+/// this callback function executes.
+///
+/// \param window - the GLFW Window that is producing the callback.
+/// \param width - the new width of the window.
+/// \param height - the new height of the window.
+///
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // Set the OpenGL viewport to be that of the new window size.
+    glViewport(0, 0, width, height);
+}
