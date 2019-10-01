d9e69e985e9d0c8875d2eddf491b97eeac8719f4
Wingman953 <nikolaosflabouris@gmail.com>
2019-10-01 19:17:30 +0930

    Moved and reformated camera class

--- Libraries/Includes/Camera/camera.h
+++ Libraries/Includes/Camera/camera.h
@@ -22,7 +22,6 @@ const float SPEED       =  2.5f;
 const float SENSITIVITY =  0.1f;
 const float ZOOM        =  45.0f;
 
-
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
 {
@@ -42,7 +41,12 @@ public:
     float Zoom;
 
     // Constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), 
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           float yaw = YAW, float pitch = PITCH)
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+        MovementSpeed(SPEED),
+        MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = position;
         WorldUp = up;
@@ -50,8 +54,20 @@ public:
         Pitch = pitch;
         updateCameraVectors();
     }
+
     // Constructor with scalar values
-    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(float posX,
+           float posY,
+           float posZ,
+           float upX,
+           float upY,
+           float upZ,
+           float yaw,
+           float pitch)
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+        MovementSpeed(SPEED),
+        MouseSensitivity(SENSITIVITY),
+        Zoom(ZOOM)
     {
         Position = glm::vec3(posX, posY, posZ);
         WorldUp = glm::vec3(upX, upY, upZ);
@@ -92,10 +108,14 @@ public:
         // Make sure that when pitch is out of bounds, screen doesn't get flipped
         if (constrainPitch)
         {
-            if (Pitch > 89.0f)
-                Pitch = 89.0f;
-            if (Pitch < -89.0f)
-                Pitch = -89.0f;
+            if (Pitch > 89.5f)
+            {
+                Pitch = 89.5f;
+            }
+            if (Pitch < -89.5f)
+            {
+                Pitch = -89.5f;
+            }
         }
 
         // Update Front, Right and Up Vectors using the updated Euler angles
@@ -106,11 +126,17 @@ public:
     void ProcessMouseScroll(float yoffset)
     {
         if (Zoom >= 1.0f && Zoom <= 45.0f)
+        {
             Zoom -= yoffset;
+        }
         if (Zoom <= 1.0f)
+        {
             Zoom = 1.0f;
+        }
         if (Zoom >= 45.0f)
+        {
             Zoom = 45.0f;
+        }
     }
 
 private:
