c256ae85f200deeef0d7092fe1a47e9def43f09b
Wingman953 <nikolaosflabouris@gmail.com>
2019-11-12 13:33:22 +1030

    Adding model loading libraries

---
+++ Libraries/build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user_pc/Downloads/assimp-master/assimp-master/contrib/zlib
+# Build directory: E:/Documents/GitHub/LearningOpenGL/Libraries/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
